609820d7f0ed2045c73afe8c80bee8c65e60d356
obafisayo-alx <sekaniisjoy@gmail.com>
2023-11-14 22:29:07 +0100

    edited the read_dets function

--- read_dets.c
+++ read_dets.c
@@ -1,37 +1,36 @@
 #include "hsh.h"
 
-
 /**
  * read_dets - This is used to get the details
  * @dets: This is the shell details
  * Return: This returns the size of the line
-*/
+ */
 bool read_dets(dets_t *dets)
 {
-    if (dets->from_terminal)
-	   write(STDERR_FILENO, "$ ", 2);
+	char *temp;
+	if (dets->from_terminal)
+		write(STDERR_FILENO, "$ ", 2);
 
-    dets->linenom += 1;
+	dets->linenom += 1;
 
-    char *line = _getline(dets->filenom);
-    if (!line) {
-	   return false;
-    }
+	char *line = _getline(dets->filenom);
+	if (!line) {
+		return false;
+	}
 
-    while (line[_strlen(line) - 1] == '\\' && _getline(dets->filenom))
-    {
-	   if (dets->from_terminal)
-		  write(STDERR_FILENO, "> ", 2);
-	   dets->linenom += 1;
+	while (_strlen(line) > 0 && line[_strlen(line) - 1] == '\\')
+	{
+		if (dets->from_terminal)
+			write(STDERR_FILENO, "> ", 2);
+		dets->linenom += 1;
 
-	   char *temp = line;
-	   line = strjoin(NULL, "", temp, dets->line);
-	   free(temp);
-	   free(dets->line);
-    }
+		line[_strlen(line) - 1] = '\0';
+		temp = line;
+		line = strjoin(NULL, "", temp, _getline(dets->filenom));
+		free(temp);
+	}
 
-    dets->line = line;
+	dets->line = line;
 
-    return true;
+	return true;
 }
-
