2d6dca2febf03cffaf53f892e4b6e51fdad96bc4
obafisayo-alx <sekaniisjoy@gmail.com>
2023-11-16 13:54:08 +0100

    commit

--- read_dets.c
+++ read_dets.c
@@ -1,5 +1,5 @@
 #include "hsh.h"
-
+#include <stdio.h>
 
 /**
  * read_dets - This is used to get the details
@@ -8,40 +8,51 @@
 */
 bool read_dets(dets_t *dets)
 {
-	char *temp = NULL, *line = NULL;
-	ssize_t backslash_pos, old_pos = 1;
-	
-	if (dets->from_terminal)
-		write(STDERR_FILENO, "$ ", 2);
+    char *temp = NULL, *line = NULL;
+    ssize_t backslash_pos, old_pos = 1;
 
-	dets->linenom += 1;
+    if (dets->from_terminal)
+        write(STDERR_FILENO, "$ ", 2);
 
-	line = getline(dets->filenom);
-	if (!line) {
-		return (false);
-	}
+    dets->linenom += 1;
 
-	while (_strlen(line) > 0 && line[_strlen(line) - 2] == '\\')
-	{
-		if (dets->from_terminal)
-		{
-			write(STDOUT_FILENO, "> ", 2);
-		}
-		dets->linenom += 1;
+    size_t size = 0;
+    line = NULL;
 
-		line[_strlen(line) - 2] = '\0';
-		temp = line;
-		line = strjoin(NULL, "", temp, getline(dets->filenom));
-	}
-	temp = line;
-	while ((backslash_pos = _strchr(temp, '\\')) != -1)
-	{
-		old_pos -= 1;
-		line[old_pos + backslash_pos] = '\0';
-		temp += backslash_pos + 1;
-		line = strjoin(NULL, "",line, temp);
-		old_pos += backslash_pos + 1;
-	}
-	dets->line = line;
-	return (true);
+    if (getline(&line, &size, dets->filenom) == -1)
+    {
+        free(line);
+        return false;
+    }
+
+    while (_strlen(line) > 0 && line[_strlen(line) - 2] == '\\')
+    {
+        if (dets->from_terminal)
+        {
+            write(STDOUT_FILENO, "> ", 2);
+        }
+        dets->linenom += 1;
+
+        line[_strlen(line) - 2] = '\0';
+        temp = line;
+        if (getline(&line, &size, dets->filenom) == -1)
+        {
+            free(temp);
+            return false;
+        }
+        line = strjoin(NULL, "", temp, line);
+    }
+
+    temp = line;
+    while ((backslash_pos = _strchr(temp, '\\')) != -1)
+    {
+        old_pos -= 1;
+        line[old_pos + backslash_pos] = '\0';
+        temp += backslash_pos + 1;
+        line = strjoin(NULL, "", line, temp);
+        old_pos += backslash_pos + 1;
+    }
+
+    dets->line = line;
+    return true;
 }
