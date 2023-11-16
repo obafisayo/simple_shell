9d0d73eadc2ee02f54ca9eea298d29a31f3d271f
obafisayo-alx <sekaniisjoy@gmail.com>
2023-11-16 04:22:45 +0100

    v

--- exec.c
+++ exec.c
@@ -8,7 +8,6 @@
 */
 int exec(dets_t *dets)
 {
-	{
 	if (_strchr(*dets->tokens, '/'))
 	{
 		dets->exe = _strdup(*dets->tokens);
@@ -19,41 +18,32 @@ int exec(dets_t *dets)
 		dets->path = str_to_list(dict_get_value(dets->envt, "PATH"), ':');
 		dets->exe = get_path(dets, dets->path);
 	}
-
 	if (dets->exe && access(dets->exe, X_OK) == 0)
-	{
-		printf("this is the exec %s\n\n", dets->exe);
-		return _exec(dets);
-	}
-
+		return (_exec(dets));
 	if (dets->exe)
 	{
 		perrorl_default(*dets->argv, dets->linenom, "Permission denied",
-					*dets->tokens, NULL);
+				*dets->tokens, NULL);
 		dets->status = 126;
 	}
 	else
 	{
 		perrorl_default(*dets->argv, dets->linenom, "not found",
-					*dets->tokens, NULL);
+				*dets->tokens, NULL);
 		dets->status = 127;
 	}
-
-	free(dets->exe);
-	dets->exe = NULL;
-
-	return dets->status;
+	return (dets->status);
 }
 
 /**
  * _exec - This is used to fork and execute the current command
  * @dets: This is the shell details
  * Return: This is the exit status of the child process
- */
+*/
 int _exec(dets_t *dets)
 {
 	char *exec, **av, **envt;
-
+	
 	switch (fork())
 	{
 	case 0:
@@ -61,14 +51,17 @@ int _exec(dets_t *dets)
 		av = dets->tokens;
 		envt = dict_to_envt(dets->envt);
 
-		free(exec);
+		dets->exe = NULL;
+		dets->tokens = NULL;
+		free_dets(dets);
 
 		execve(exec, av, envt);
 		perror(*av);
 
 		if (dets->file)
 			close(dets->filenom);
-
+		
+		free(exec);
 		free_tokens(&av);
 		free_tokens(&envt);
 		exit(EXIT_FAILURE);
@@ -84,7 +77,6 @@ int _exec(dets_t *dets)
 		dets->status = WEXITSTATUS(dets->status);
 		break;
 	}
-
 	free(dets->exe);
 	dets->exe = NULL;
 
