3be811be93e050cf5e20dc30565183cd23e4e43d
obafisayo-alx <sekaniisjoy@gmail.com>
2023-11-16 13:16:00 +0100

    lets go back

--- hsh.h
+++ hsh.h
@@ -23,15 +23,10 @@
 extern char **environ;
 
 bool read_dets(dets_t *dets);
-int parse_dets(dets_t *dets);
-void free_tokens(char ***tokens);
 int exec(dets_t *dets);
 int _exec(dets_t *dets);
 void _sigint(int signal);
 char *get_path(dets_t *dets, list_t *path);
-int execute_file(char **args, struct stat *st, char **environ, char **argv);
-bool check_file_status(char *pathname, struct stat *st, char **argv);
-void process(dets_t *dets);
 void free_word_arr(char **words_arr);
 
 #endif /* _HSH_H_*/
