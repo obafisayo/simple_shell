503e99e2bf8ef56b8b9fce64921ca71c65f7830a
ReenaALX <m9991333@gmail.com>
2023-11-17 15:41:41 -0800

    arrjoin.c

--- arrjoin.c
+++ arrjoin.c
@@ -10,33 +10,33 @@
 char **arrjoin(char **arr1, char **arr2)
 {
 	char **new;
-	size_t arr1_len = 0, arr2_len = 0, new_length;
+	size_t arr1_len = 0, arr2_len = 0, new_len;
 
 	if (arr1)
 	{
-		while (arr1[arr1_length])
-			++arr1_length;
+		while (arr1[arr1_len])
+			++arr1_len;
 	}
 	if (arr2)
 	{
-		while (arr2[arr2_length])
-			++arr2_length;
+		while (arr2[arr2_len])
+			++arr2_len
 	}
-	new = malloc(sizeof(char *) * (arr1_len + arr2_length + 1));
+	new = malloc(sizeof(char *) * (arr1_len + arr2_len + 1));
 	if (!new)
 		return (NULL);
 
-	new_length = 0;
+	new_len = 0;
 	if (arr1)
 	{
 		while (*arr1)
-			new[new_length++] = _strdup(*arr1++);
+			new[new_len++] = _strdup(*arr1++);
 	}
 	if (arr2)
 	{
 		while (*arr2)
-			new[new_length++] = _strdup(*arr2++);
+			new[new_len++] = _strdup(*arr2++);
 	}
-	new[new_length] = NULL;
+	new[new_len] = NULL;
 	return (new);
 }
