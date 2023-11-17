f33430b7e0d5ee1586a358773283bf3e72c817e4
ReenaALX <m9991333@gmail.com>
2023-11-17 15:47:46 -0800

    __exec.c

--- __exec.c
+++ __exec.c
@@ -7,7 +7,7 @@
  */
 int __exec(dets_t *dets)
 {
-	char *exec, **args = dets->tokens + 1, **env = NULL;
+	char *execute, **args = dets->tokens + 1, **env = NULL;
 
 	if (!*args)
 		return ((dets->status = EXIT_SUCCESS));
@@ -19,30 +19,30 @@ int __exec(dets_t *dets)
 	{
 		free_list(&dets->path);
 		dets->path = str_to_list(get_dict_val(dets->env, "PATH"), ':');
-		exec = search_path(dets, dets->path);
+		execute = search_path(dets, dets->path);
 	}
 	else
 	{
-		exec = _strdup(*args);
+		execute = _strdup(*args);
 	}
 	dets->tokens -= 1;
 
-	if (access(exec, X_OK) == 0)
+	if (access(execute, X_OK) == 0)
 	{
 		env = dict_to_env(dets->env);
 
 		free_dets(dets);
-		execve(exec, args, env);
+		execve(execute, args, env);
 		perrorl_default(*dets->argv, dets->lineno, "Not found",
 				*dets->tokens, *args, NULL);
-		free(exec);
+		free(execute);
 		free_tokens(&args);
 		free_tokens(&env);
 		exit(127);
 	}
 	perrorl_default(*dets->argv, dets->lineno, "Permission denied",
 			*dets->tokens, *args, NULL);
-	free(exec);
+	free(execute);
 	free_tokens(&args);
 	free_dets(dets);
 	exit(126);
